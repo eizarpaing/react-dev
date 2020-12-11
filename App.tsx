f4008900a1d427da073381d351c7aa1caed86fd3
chan nyein tun <channyeintun@yahoo.com>
2020-12-11 16:51:57 +0630

    chan

--- App.tsx
+++ App.tsx
@@ -7,68 +7,74 @@ export default function App() {
   const logo = require("./assets/favicon.png");
   const screen = require("./assets/screen.png");
 
-  const postTestApi = () => {
+  const postTestApi = async () => {
     alert("post test");
-    fetch('https://jsonplaceholder.typicode.com/posts', {
+    let request = { title: 'testing movie 1' };
+    await fetch('https://jsonplaceholder.typicode.com/posts', {
       method: 'POST',
-      body: JSON.stringify({ title: 'this is testing'}),
-    }).then(response => {
-      alert(response);
-    }).catch(error => {
-      alert("last error" + error.toString());
-    });
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(request),
+    })
+      .then(response => {
+        alert(response.json());
+      }).catch(error => {
+        alert("last error" + error);
+      });
   }
 
   const getMoviesFromApi = () => {
     return fetch('https://reactnative.dev/movies.json')
-    .then(response => {
-      alert(response);
-      alert('Response: ' + response);
-    })
-    .catch((error) => {
-      alert(error);
-    });
+      .then(response => {
+        alert(response);
+        alert('Response: ' + response);
+      })
+      .catch((error) => {
+        alert(error);
+      });
   };
 
   return (
     <View style={styles.container}>
-      <View style={{ flex: 1, flexDirection: "column", padding: "50px"}}>
-        <img style={{width: '138px', height: '46px', marginBottom: "20px"}} src={logo}></img>
+      <View style={{ flex: 1, flexDirection: "column", padding: "50px" }}>
+        <img style={{ width: '138px', height: '46px', marginBottom: "20px" }} src={logo}></img>
         <ScrollView>
-          <View style={{paddingTop: '20px'}}>
+          <View style={{ paddingTop: '20px' }}>
             <Form>
               <Form.Group>
                 <Form.Label style={{ fontSize: "14px" }}>name</Form.Label>
                 <View style={{ marginBottom: "20px" }}>
-                  <Form.Control type="text" placeholder="enter name" onChange={({target}) => setName(target.value)} value={name} style={{ height: "35px", padding: "0px 10px" }}/>
+                  <Form.Control type="text" placeholder="enter name" onChange={({ target }) => setName(target.value)} value={name} style={{ height: "35px", padding: "0px 10px" }} />
                 </View>
               </Form.Group>
               <Form.Group>
                 <Form.Label style={{ fontSize: "14px" }}>type</Form.Label>
                 <View style={{ marginBottom: "20px" }}>
-                  <Form.Control as="select" placeholder="enter type" style={{ height: "35px", padding: "0px 10px"}}>
+                  <Form.Control as="select" placeholder="enter type" style={{ height: "35px", padding: "0px 10px" }}>
                     <option>a</option>
                     <option>b</option>
                   </Form.Control>
                 </View>
               </Form.Group>
-              <Button onClick={postTestApi} variant="primary" type="submit" style={{ height: "40px", width: "100%", borderRadius: "20px", backgroundColor: "#333333", color: "#FFFFFF", fontSize: "14px", marginTop: "10px" }}>
-              Create
+              <Button onClick={postTestApi} variant="primary" style={{ height: "40px", width: "100%", borderRadius: "20px", backgroundColor: "#333333", color: "#FFFFFF", fontSize: "14px", marginTop: "10px" }}>
+                Side Create
               </Button>
             </Form>
-	        </View>
+          </View>
         </ScrollView>
       </View>
       <View style={styles.secondContainer}>
-        <View style={{padding: "50px", position: "fixed"}}>
-          <Text style={{fontSize: "25px", fontWeight: "bold", color: "#fff"}}>
-          Testing Page{"\n"}
+        <View style={{ padding: "50px", position: "fixed" }}>
+          <Text style={{ fontSize: "25px", fontWeight: "bold", color: "#fff" }}>
+            Testing Page{"\n"}
           Main Page
           </Text>
-          <button onClick={postTestApi} type="submit" style={{ height: "40px", width: "100%", borderRadius: "20px", backgroundColor: "#333333", color: "#FFFFFF", fontSize: "14px", marginTop: "10px" }}>
-            Create
+          <button onClick={postTestApi} style={{ height: "40px", width: "100%", borderRadius: "20px", backgroundColor: "#333333", color: "#FFFFFF", fontSize: "14px", marginTop: "10px" }}>
+            Main Create
          </button>
-          <img style={{paddingTop: "20px", alignSelf: "center", width: "70%", height: "70%"}} src={screen}></img>
+          <img style={{ paddingTop: "20px", alignSelf: "center", width: "70%", height: "70%" }} src={screen}></img>
         </View>
       </View>
     </View>
